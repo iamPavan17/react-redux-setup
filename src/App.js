9b6f2ed3751d8c490b6df4f164b1b98d87e8c040
Pavan S <pavan@bambu.co>
2023-03-30 16:04:48 +0530

    ✨ feat: addTodo

--- src/App.js
+++ src/App.js
@@ -1,9 +1,40 @@
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+
 import "./App.css";
+import { generateGuid } from "./helpers";
+import { addTodo } from "./redux/actions/todoActions";
 
 function App() {
+  const [value, setValue] = useState("");
+  const dispatch = useDispatch();
+
+  const handleChange = (e) => {
+    setValue(e.target.value);
+  };
+
+  const handleSubmit = () => {
+    const payload = {
+      id: generateGuid(),
+      value,
+    };
+
+    // setting the text value to empty
+    setValue("");
+
+    // dispatching an action
+    dispatch(addTodo(payload));
+  };
+
   return (
     <div className="App">
       <h1>Hello React!</h1>
+      <input value={value} onChange={handleChange} />
+      <button onClick={handleSubmit}>Save</button>
+
+      {todos.map((todo) => (
+        <p key={todo.id}>{todo.value}</p>
+      ))}
     </div>
   );
 }
---
+++ src/helpers.js
@@ -0,0 +1,8 @@
+function generateGuid() {
+  return (
+    Math.random().toString(36).substring(2, 15) +
+    Math.random().toString(36).substring(2, 15)
+  );
+}
+
+export { generateGuid };
